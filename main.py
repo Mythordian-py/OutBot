841c3b96336e8fd21829bcca65131d4b129c45f0
OutDev <mythordian@proton.me>
2026-08-19 12:37:06 +0500

    Update main.py
    Removed /say, /dm and /hello because they are in the general cog

--- main.py
+++ main.py
@@ -50,23 +50,6 @@ async def on_ready():
 # |================|
 
 
-@bot.tree.command(
-    name="hello",
-    description="It pings you & says hello!",
-
-)
-async def hello(interaction):
-    """
-    This command greets the user.
-    """
-    await interaction.response.send_message(f"Hello, {interaction.user.mention}!")
-
-
-@bot.tree.command(
-    name="outmyth",
-    description="Shows OutMyth's Discord & YouTube links and OutMyth's owners",
-
-)
 async def outmyth(interaction):
     """
     Useful information about OutMyth (Explains what OUtMyth is).
@@ -126,68 +109,6 @@ async def rules(interaction: str):
 
 - :handshake: Be kind, respectful, and helpful to everyone.
     {interaction.user.mention}""")
-
-
-@bot.tree.command(
-    name="dm",
-    description="Dms the user. Please make sure you have Dms turned on.",
-
-)
-async def dm(interaction, msg: str):
-    """
-    THe command Dms the user who triggered the command.
-    """
-    if len(msg) > 2000:
-        await interaction.response.send_message(
-            """Error 413! Your message was more than characters 2000. This means your message is too long to send. Please make your 
-        message shorter""",
-            ephemeral=True,
-        )
-        return
-
-    try:
-        await interaction.user.send(f"Dm: ||{msg}||")
-        await interaction.response.send_message("Check your Dms!", ephemeral=True)
-
-    except discord.Forbidden:
-        await interaction.response.send_message(
-            """Error 403! I could not send you a Dm. This is because you have them turned off. Please turn them on to allow me to send
-        you a dm""",
-            ephemeral=True,
-        )
-
-    except Exception:
-        logging.exception("Unexpected error in /dm")
-
-
-@bot.tree.command(
-    name="say",
-    description="You tell the Bot what to say!",
-
-)
-async def say(interaction, say: str):
-    """
-    You tell the bot what to say.
-    """
-    if len(say) > 2000:
-        await interaction.response.send_message(
-            "Error 413! Your message was too long. Please make it shorter. To allow me to send you a DM.",
-            ephemeral=True,
-        )
-        return
-
-    try:
-        await interaction.response.send_message(
-            f"{interaction.user.mention} told me to say: ||{say}||"
-        )
-
-    except discord.HTTPException as e:
-        await interaction.response.send_message(
-            f"Error{e.status}! Discord API failure", ephemeral=True
-        )
-
-    except Exception:
-        logging.exception("Unexpected error in /say")
 
 
 @bot.tree.command(
