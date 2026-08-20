2669ea2346f85f0cdac2337524931a89ba00b49e
OutDev <306765009+OutMyth-Dev@users.noreply.github.com>
2026-08-20 15:17:46 +0500

    Update links.py
    Basic cog structure added to the Cog links.py

--- cogs/links.py
+++ cogs/links.py
@@ -1,1 +1,12 @@
+import discord
 
+from discord.ext import commands
+
+class LinksCommands(commands.cog):
+  def __init__(self,bot):
+    self.bot = bot
+
+
+
+async def setup(bot):
+  await bot.add_cog(Links(bot))
