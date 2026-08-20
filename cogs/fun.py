12feaf3b4589533ffe3baeceab13c9e9d854b5ee
OutDev <306765009+OutMyth-Dev@users.noreply.github.com>
2026-08-20 17:40:10 +0500

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -4,19 +4,19 @@ from discord.ext import commands
 
 class FunCommands(commands.Cog):
   
-  def __init__(self, bot):
-    self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-  @discord.tree.command(
-    name="rickroll",
-    description="Don't do it...",
+    @discord.tree.command(
+        name="rickroll",
+        description="Don't do it...",
 )
   
-  async def rickroll(self, interaction: discord.Interaction):
+    async def rickroll(self, interaction: discord.Interaction):
     """Sends a youtube link to rickroll the user."""
-    await interaction.response.send_message(
+        await interaction.response.send_message(
         "CLICK ME ---> ||<https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1>||",
         ephemeral=True)
     
 async def setup(bot):
-  await bot.add_cog(FunCommands(bot))
+    await bot.add_cog(FunCommands(bot))
