c85e743ecb7f5e3118c7233b3c2f0874db556cb4
OutDev <306765009+OutMyth-Dev@users.noreply.github.com>
2026-08-20 17:39:15 +0500

    Update general.py

--- cogs/general.py
+++ cogs/general.py
@@ -6,86 +6,86 @@ from emojis import emojis
 
 class GeneralCommands(commands.Cog):
   
-  def __init__(self, bot):
-    self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-  @discord.app_commands.command(
-    name="hello",
-    description="It pings you & says hello!",
+      @discord.app_commands.command(
+        name="hello",
+        description="It pings you & says hello!",
   )
   
-  async def hello(self, interaction: discord.Interaction):
-      await interaction.response.send_message(f"Hello, {interaction.user.mention}!")
+    async def hello(self, interaction: discord.Interaction):
+        await interaction.response.send_message(f"Hello, {interaction.user.mention}!")
   
-  @discord.app_commands.command(
+    @discord.app_commands.command(
     name="dm",
     description="Dms the user. Please make sure you have Dms turned on.",
 )
 
-  async def dm(self, interaction: discord.Interaction, msg: str):
-      if len(msg) > 1999:
-          await interaction.response.send_message(
+    async def dm(self, interaction: discord.Interaction, msg: str):
+        if len(msg) > 1999:
+            await interaction.response.send_message(
               """Your message was more than characters 2000. This means your message is too long to send. Please make your message 
               shorter""",
               ephemeral=True,
           )
-          return
+            return
   
-      try:
-          await interaction.user.send(f"Dm: ||{msg}||")
-          await interaction.response.send_message("Check your Dms!", ephemeral=True)
+        try:
+            await interaction.user.send(f"Dm: ||{msg}||")
+            await interaction.response.send_message("Check your Dms!", ephemeral=True)
   
-      except discord.Forbidden:
-          await interaction.response.send_message(
+        except discord.Forbidden:
+            await interaction.response.send_message(
               """I could not send you a Dm. This is because you have them turned off. Please turn them on to allow me to send you a dm""",
               ephemeral=True,
           )
   
-      except Exception:
-          logging.exception("Unexpected error in /dm")
-    
-  @discord.app_commands.command(
-      name="say",
-      description="You tell the Bot what to say!",
+        except Exception:
+            logging.exception("Unexpected error in /dm")
+  
+      @discord.app_commands.command(
+          name="say",
+          description="You tell the Bot what to say!",
   )
 
-  async def say(self, interaction: discord.Interaction, say: str):
-      if len(say) > 1999:
-          await interaction.response.send_message(
+    async def say(self, interaction: discord.Interaction, say: str):
+        if len(say) > 1999:
+            await interaction.response.send_message(
               "Your message was too long. Please make it shorter. To allow me to send you a DM.",
               ephemeral=True,
           )
-          return
+            return
   
-      try:
-          await interaction.response.send_message(
+        try:
+            await interaction.response.send_message(
               f"{interaction.user.mention} told me to say: ||{say}||"
           )
   
-      except discord.HTTPException as e:
-          await interaction.response.send_message(
+        except discord.HTTPException as e:
+            await interaction.response.send_message(
                "Discord API failure", ephemeral=True
           )
   
-      except Exception:
-          logging.exception("Unexpected error in /say")
+        except Exception:
+            logging.exception("Unexpected error in /say")
 
 
-@discord.app_commands.command(
-    name="ping",
-    description="Pings you",
+    @discord.app_commands.command(
+        name="ping",
+        description="Pings you",
 )
 
-async def ping(self, interaction: discord.Interaction):
-    await interaction.response.send_message(f"{interaction.user.mention}")
+    async def ping(self, interaction: discord.Interaction):
+        await interaction.response.send_message(f"{interaction.user.mention}")
 
-@dsicord.app_commands.command(
-    name="poll",
-    description="Create a new poll.",
+    @dsicord.app_commands.command(
+        name="poll",
+        description="Create a new poll.",
 )
-  async def poll(interaction, title: str, question: str):
-    """20 reactions to allow the user to pick a reaction of their choice. 20 reactions is the max amount of reactions aDiscord message
-    can have.The poll title and question are both strings."""
+      async def poll(interaction, title: str, question: str):
+        """20 reactions to allow the user to pick a reaction of their choice. 20 reactions is the max amount of reactions aDiscord message
+        can have.The poll title and question are both strings."""
     embed = discord.Embed(title=title, description=question)
     await interaction.response.send_message(embed=embed)
     poll_msg = await interaction.original_response()
